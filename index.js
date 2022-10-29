80fb012d4c75bd573f98a3e6044f5e503d02669b
ANKIT MISHRA <ankitmrmishra1118@gmail.com>
2022-10-29 19:01:08 +0530

    the final commit

--- index.js
+++ index.js
@@ -37,6 +37,42 @@ function pauseSong(){
 
     audio.pause()
 }
+
+function prevsong(){
+ songIndex-- 
+if(songIndex < 0 ){
+    songIndex = songs.length - 1
+} 
+songLoad(songs[songIndex]) 
+audio.play()
+
+}
+function nextsong(){
+    songIndex++ 
+if(songIndex > (songs.length -1) ){
+    songIndex = 0
+} 
+songLoad(songs[songIndex]) 
+audio.play()
+    
+
+
+}
+ 
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration ) * 100
+    progress.style.width = `${progressPercent}%`
+}
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+
+}
+
+
 //event listensers
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play')
@@ -47,3 +83,15 @@ playBtn.addEventListener('click', () => {
         playSong()
     }
 })
+
+//change song evnets
+
+prevBtn.addEventListener('click' , prevsong)
+nextBtn.addEventListener('click' , nextsong)
+
+
+audio.addEventListener('timeupdate' , updateProgress )
+
+progressContainer.addEventListener('click' , setProgress)
+
+audio.addEventListener('ended' , nextsong)
